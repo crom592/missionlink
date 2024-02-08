c91b295c76eff9bf53eba29da97476fbd050ae31
crom <holyfavor@gmail.com>
2024-02-08 14:57:33 +0900

    (chore) change setting deploy

--- missionlink/wsgi.py
+++ missionlink/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'missionlink.settings')
 
 application = get_wsgi_application()
+app = application
